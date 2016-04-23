ec1080ad49fe02d7569e456bc93b5d1669c62418
alexdecapri <alexdecapri@gmail.com>
2016-04-23 10:05:21 -0600

    almost through with blog feed section

---
+++ app/components/Blog-intro-container/Blog-image/Blog-image.js
@@ -0,0 +1,9 @@
+require('./Blog-image.css');
+import React from 'react';
+import BlogLink from './Blog-link/Blog-link';
+
+class BlogIntro extends React.Component {
+
+}
+
+export default BlogIntro
--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,3 +1,28 @@
 require('./Home.css');
 import React from 'react';
-import Blog-intro-container from '../Blog-intro-container/Blog-intro-container';
+import BlogIntro from '../Blog-intro-container/Blog-intro-container';
+import {getUserInfo, getPosts} from '../../utils/helpers';
+
+class Home extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      cardData: [],
+      userInfo: {},
+      intro: ''
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <BlogIntro />
+      </div>
+    )
+  }
+
+}
+
+export default Home
