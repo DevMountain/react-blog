2f4ff23ac0a8a1d7cb89ebf408df8a4ea9561085
alexdecapri <alexdecapri@gmail.com>
2016-04-23 12:14:22 -0600

    started redux

--- app/App.js
+++ app/App.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import { Router, hashHistory  } from 'react-router';
 import routes from './config/routes';
 
+import Provider from 'react-redux';
+import createStore from 'redux';
+import reducer from './Redux/Like';
+
 ReactDOM.render(
     <Router history={ hashHistory }>{routes}</Router>,
     document.getElementById('app')
